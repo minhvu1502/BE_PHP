70a32a517532cac73487ce07b9328aa383bea78a
minhvu1502 <43747199+minhvu1502@users.noreply.github.com>
2020-09-16 14:57:43 +0700

    tao moi project

---
+++ app/Http/Middleware/CORS.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class CORS
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', 'http://127.0.0.1:8000/')
+            ->header('Access-Control-Allow-Methods', '*')
+            ->header('Access-Control-Allow-Credentials', 'true')
+            ->header('Access-Control-Allow-Headers', 'X-CSRF-Token');
+    }
+}
