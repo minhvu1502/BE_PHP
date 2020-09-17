74308c7d9725c6b1022388547e4f7e646d09d34b
minhvu1502 <43747199+minhvu1502@users.noreply.github.com>
2020-09-17 21:10:43 +0700

    thêm mới authen

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,53 +13,64 @@ class UserController extends Controller
 {
     private $user;
 
-    public function __construct(User $user){
+    public function __construct(User $user)
+    {
         $this->user = $user;
     }
-   
-    public function register(Request $request){
-        $user = DB::table('users')->where('email', $request->get('email'))->first();
+
+    public function register(Request $request)
+    {
+        $user = DB::table('users')->where('username', $request->get('username'))->first();
+
         if ($user) {
             return response()->json([
-                'status'=> 0,
-                'message'=> 'Tài khoản đã tồn tại',
+                'status' => 0,
+                'message' => 'Tài khoản đã tồn tại',
             ]);
         }
         $user = $this->user->create([
-          'name' => $request->get('name'),
-          'email' => $request->get('email'),
-          'password' => Hash::make($request->get('password'))
+            'username' => $request->get('username'),
+            'name' => $request->get('name'),
+            'email' => $request->get('email'),
+            'password' => Hash::make($request->get('password'))
         ]);
 
         return response()->json([
-            'status'=> 200,
-            'message'=> 'Tạo mới thành công',
-            'data'=>$user
+            'status' => 200,
+            'message' => 'Tạo mới thành công',
+            'data' => $user
         ]);
     }
-    
-    public function login(Request $request){
-        $credentials = $request->only('email', 'password');
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('username', 'password');
         $token = null;
         try {
-           if (!$token = JWTAuth::attempt($credentials)) {
-            return response()->json([
-                'status' => 0,
-                'message' => 'invalid_email_or_password'
-            ]);
-           }
+            if (!$token = JWTAuth::attempt($credentials)) {
+                return response()->json([
+                    'status' => 0,
+                    'message' => 'invalid_username_or_password'
+                ]);
+            }
         } catch (JWTAuthException $e) {
             return response()->json(['failed_to_create_token'], 500);
         }
         return response()->json([
             'status' => 200,
-            'message'=> 'Login successfully',
+            'message' => 'Login successfully',
             'data' => compact('token')
         ]);
     }
 
-    public function getUserInfo(Request $request){
+    public function getUserInfo(Request $request)
+    {
         $user = JWTAuth::toUser($request->token);
-        return response()->json(['result' => $user]);
+        if ($user) {
+            return response()->json([
+                'data' => $user,
+                'status' => 200
+            ]);
+        }
     }
 }
--- app/Http/Middleware/VerifyJWTToken.php
+++ app/Http/Middleware/VerifyJWTToken.php
@@ -16,12 +16,18 @@ class VerifyJWTToken
     public function handle($request, Closure $next)
     {
         try {
-            $user = JWTAuth::toUser($request->input('token'));
+            $user = JWTAuth::toUser($request->header('token'));
         }catch (JWTException $e) {
             if($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException) {
-                return response()->json(['token_expired'], $e->getStatusCode());
+                return response()->json([
+                    'status' => 498,
+                    'message'=> 'token_expired'
+                ]);
             }else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException) {
-                return response()->json(['token_invalid'], $e->getStatusCode());
+                return response()->json([
+                    'status' => 401,
+                    'message' => 'token_invalid'
+                ]);
             }else{
                 return response()->json(['error'=>'Token is required']);
             }
