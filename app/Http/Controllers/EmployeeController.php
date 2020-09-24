331a3a173c63ce9f39f4463d8ed05e3072a65029
minhvu1502 <43747199+minhvu1502@users.noreply.github.com>
2020-09-24 15:58:03 +0700

    update

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Employee;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class EmployeeController extends Controller
+{
+    private $employee;
+
+    public function __construct(Employee $employee)
+    {
+        $this->employee = $employee;
+    }
+
+    public function filter(Request $request)
+    {
+        try {
+            $employee = Employee::all();
+            return response()->json([
+                'status' => 200,
+                'data' => $employee,
+                'message' => 'Lấy dữ liệu thành công'
+            ], 200);
+        } catch (\Throwable $e) {
+            return response()->json([
+                'status' => 500,
+                'message' => $e
+            ], 500);
+        }
+    }
+
+    public function create(Request $request)
+    {
+        try {
+            $employee = DB::table('employees')->where('code', $request->get('code'))->first();
+            if ($employee) {
+                return response()->json([
+                    'status' => 500,
+                    'message' => 'Nhân viên đã tồn tại'
+                ], 500);
+            }
+            $employee = $this->employee->create([
+                'code' => $request->get('code'),
+                'name' => $request->get('name'),
+                'status' => $request->get('status'),
+                'sex' => $request->get('sex'),
+                'dateOfBirth' => $request->get('dateOfBirth'),
+                'phone' => $request->get('phone'),
+                'Hometown_Id' => $request->get('Hometown_Id'),
+            ]);
+            return response()->json([
+                'status' => 200,
+                'message' => 'Tạo mới thành công',
+                'data' => $employee
+            ], 200);
+        } catch (\Throwable $e) {
+            report($e);
+            return response()->json([
+                'status' => 500,
+                'message' => $e
+            ], 500);
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
+        try {
+            $employee = DB::table('employees')->where('id', $id);
+            if (!$employee) {
+                return response()->json([
+                    'status' => 500,
+                    'message' => 'Không thể cập nhật'
+                ]);
+            } else {
+                $employee = DB::table('employees')->where('id', $id)->update([
+                    'name' => $request->get('name'),
+                    'status' => $request->get('status'),
+                    'sex' => $request->get('sex'),
+                    'dateOfBirth' =>$request->get('dateOfBirth'),
+                    'phone'=>$request->get('phone'),
+                    'Hometown_Id' =>$request->get('Hometown_Id'),
+                    'updated_at' =>Carbon::now()
+                ]);
+                return response()->json([
+                    'status' => 200,
+                    'message' => 'Cập nhật thành công',
+                    'data' => [
+                        'id' => $id,
+                        'name' => $request->get('name')
+                    ]
+                ], 200);
+            }
+        } catch (\Throwable $e) {
+            report($e);
+            return response()->json([
+                'status' => 500,
+                'message' => $e,
+            ], 500);
+        }
+    }
+
+    public function Detail($id)
+    {
+        try {
+            $employee = DB::table('employees')->find($id);
+            if ($employee) {
+                return response()->json([
+                    'status' => 200,
+                    'message' => 'Lấy dữ liệu thành công',
+                    'data' => $employee
+                ], 200);
+            } else {
+                return response()->json([
+                    'status' => 500,
+                    'message' => 'Không tìm thấy'
+                ], 500);
+            }
+        } catch (\Throwable $e) {
+            report($e);
+            return response()->json([
+                'status' => 500,
+                'message' => $e
+            ], 500);
+        }
+    }
+
+    public function delete(Request $request)
+    {
+        try {
+            $listId = $request->get('listId');
+            $count = count($listId);
+            if ($count > 0) {
+                DB::table('employees')->whereIn('id', $listId)->delete();
+                return response()->json([
+                    'status' => 200,
+                    'message' => 'Xóa dữ liệu thành công',
+                    'data' => $listId
+                ], 200);
+            } else {
+                return response()->json([
+                    'status' => 500,
+                    'message' => 'Xóa dữ liệu không thành công'
+                ], 500);
+            }
+        } catch (\Throwable $e) {
+            report($e);
+            return response()->json([
+                'status' => 500,
+                'message' => $e
+            ], 500);
+        }
+    }
+}
---
+++ database/migrations/2020_09_24_082118_create_employees_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('code');
+            $table->string('name');
+            $table->boolean('sex');
+            $table->dateTime('dateOfBirth');
+            $table->string('phone');
+            $table->boolean('status');
+            $table->foreignId('Hometown_Id')->constrained('hometowns')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
